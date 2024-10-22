c4c4ebf5b4abe9845fcf0d1ebb7d892a1b6051f1
Param Dhaliwal <veerdhaliwal22@gmail.com>
2024-10-22 10:16:25 -0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+// Existing hamburger menu functionality
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 const navLink = document.querySelectorAll(".nav-link");
@@ -14,3 +15,49 @@ function closeMenu() {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }
+
+// New typing animation functionality
+const roles = [
+    'Salesforce Developer',
+    'Front-End Developer',
+    'Back-End Developer',
+    'Technical Consultant'
+];
+
+let i = 0;
+let j = 0;
+let currentRole = '';
+let isDeleting = false;
+const speed = 150; // Typing speed
+
+function type() {
+    const dynamicText = document.querySelector('.dynamic-text');
+
+    if (i < roles.length) {
+        if (!isDeleting && j <= roles[i].length) {
+            currentRole = roles[i].substring(0, j);
+            dynamicText.innerHTML = currentRole;
+            j++;
+        }
+
+        if (isDeleting && j <= roles[i].length) {
+            currentRole = roles[i].substring(0, j);
+            dynamicText.innerHTML = currentRole;
+            j--;
+        }
+
+        if (j === roles[i].length) {
+            isDeleting = true;
+            setTimeout(type, 2000); // Wait before backspacing
+        } else if (j === 0 && isDeleting) {
+            isDeleting = false;
+            i++;
+            if (i === roles.length) {
+                i = 0;
+            }
+        }
+    }
+    setTimeout(type, isDeleting ? speed / 2 : speed);
+}
+
+document.addEventListener('DOMContentLoaded', type);
